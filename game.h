ef101273e839d05620e2d2830cf6b1b6f8710ae6
Sergei Gorkov <serggorkov@icloud.com>
2026-06-12 00:09:40 +0300

    Changed constants definition

--- game.h
+++ game.h
@@ -10,20 +10,20 @@
 #define FONT_FILE "Samson.ttf"
 #define BOMB_SPRITE "bomb_circle.png"
 
-const int FIELD_SIZE = 5;   // bloks amount in field
-const int BLOCK_SIZE = 100; // block size in pixels
+constexpr int FIELD_SIZE = 5;   // bloks amount in field
+constexpr int BLOCK_SIZE = 100; // block size in pixels
 
-const int WINDOW_SIZE = 900; // window size
+constexpr int WINDOW_SIZE = 900; // window size
 
-const int DELTA_TIME = 300; // time of dropping blocks in milliseconds
+constexpr int DELTA_TIME = 300; // time of dropping blocks in milliseconds
 
-const int DESTROY_AMOUNT = 3; // amount of blocks with same color to destroy them
+constexpr int DESTROY_AMOUNT = 3; // amount of blocks with same color to destroy them
 
-const int FRAME_SIZE = 10;                                  // size of frame to highlight selected block
-const sf::Color HIGHLIGHT_COLOR = sf::Color(128, 128, 128); // color to highlight selected block
+constexpr int FRAME_SIZE = 10;                                  // size of frame to highlight selected block
+constexpr sf::Color HIGHLIGHT_COLOR = sf::Color(128, 128, 128); // color to highlight selected block
 
-const int COLORS_COUNT = 7;
-const sf::Color COLORS[COLORS_COUNT] = {
+constexpr int COLORS_COUNT = 7;
+constexpr sf::Color COLORS[COLORS_COUNT] = {
     sf::Color::Cyan,
     sf::Color::Green,
     sf::Color::Magenta,
